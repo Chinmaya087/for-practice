73b311624035f39a7e6c874509828cd6ad471857
chinmaya087 <Chinmaya087@hotmail.com>
2025-12-25 19:25:40 +0530

    Add CHIN NEW project

---
+++ src/com/forpractice/First.java
@@ -0,0 +1,14 @@
+package com.forpractice;
+
+//import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		
+         System.out.println("Hello world");
+         //for practice
+	}
+
+}
