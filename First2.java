b4acc8e77aeafd60f9449146b6b0112ee78f0fff
chinmaya087 <Chinmaya087@hotmail.com>
2025-12-26 19:52:22 +0530

    new practice

--- First2.java
+++ First2.java
@@ -2,6 +2,6 @@ public class First2 {
     public static void main(String[] args) {
         System.out.println("Hello, Worrrtyrld!");
         //for practice 2
-        // for practice 4477
+        // for practice 447
     }
 }
